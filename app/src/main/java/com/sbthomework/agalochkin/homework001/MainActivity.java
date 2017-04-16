6b05f0ffc4d5b8e3eb3d47869e9cbfed887cff89
Alexander <balron923@gmail.com>
2017-04-16 13:02:35 +0300

    add 3 view

--- app/src/main/java/com/sbthomework/agalochkin/homework001/MainActivity.java
+++ app/src/main/java/com/sbthomework/agalochkin/homework001/MainActivity.java
@@ -3,11 +3,15 @@ package com.sbthomework.agalochkin.homework001;
 import android.animation.ValueAnimator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.animation.DecelerateInterpolator;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private TextView mFirstAnimatedView;
+    private TextView mFourAnimatedView;
+    private View mSecondAnimatedView;
+    private View mThirdAnimatedView;
     private ValueAnimator mAnimator;
 
     @Override
@@ -15,7 +19,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mFirstAnimatedView = (TextView) findViewById(R.id.anim_view);
+        mFirstAnimatedView = (TextView) findViewById(R.id.anim_view_first);
+        mSecondAnimatedView = findViewById(R.id.anim_view_second);
+        mThirdAnimatedView = findViewById(R.id.anim_view_third);
+        mFourAnimatedView = (TextView) findViewById(R.id.anim_view_four);
 
         mAnimator = ValueAnimator.ofInt(1, 10000);
         mAnimator.setRepeatMode(ValueAnimator.REVERSE);
@@ -27,9 +34,25 @@ public class MainActivity extends AppCompatActivity {
             public void onAnimationUpdate(ValueAnimator valueAnimator) {
                 Integer value = (Integer) valueAnimator.getAnimatedValue();
                 mFirstAnimatedView.getBackground().setLevel(value);
-                mFirstAnimatedView.setText(value/100+" %");
+                mFirstAnimatedView.setText(value / 100 + " %");
+
+                mFourAnimatedView.getBackground().setLevel(value);
+                mFourAnimatedView.setText(value / 100 + " %");
+
+
+                mSecondAnimatedView.getBackground().setLevel(value);
+                mThirdAnimatedView.getBackground().setLevel(value);
             }
         });
         mAnimator.start();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        mAnimator.cancel();
+        mAnimator.removeAllUpdateListeners();
+        mAnimator = null;
     }
 }
